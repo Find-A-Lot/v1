83cad644c99bac229d173d0535818313dbd672ab
Garrett Tolbert <garrett1.tolbert@gmail.com>
2019-11-13 20:23:31 -0500

    Mocks updated and some code

--- app/src/main/java/com/example/findalot/MainActivity.java
+++ app/src/main/java/com/example/findalot/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.findalot;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.findalot.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,12 +26,19 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
 
     private static final String TAG = "MainActivity";
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private TextView welcome;
+    private Button parked_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +48,37 @@ public class MainActivity extends AppCompatActivity {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
         welcome = findViewById(R.id.welcomeMsg);
+        parked_btn = findViewById(R.id.parkedBtn);
 
+
+        parked_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fusedLocationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+                Log.d(TAG, "Fetching location: ");
+
+                locationRequest = LocationRequest.create();
+                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                locationRequest.setInterval(20 * 1000);
+                locationCallback = new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        if (locationResult == null) {
+                            return;
+                        }
+                        for (Location location : locationResult.getLocations()) {
+                            if (location != null) {
+                                double wayLatitude = location.getLatitude();
+                                double wayLongitude = location.getLongitude();
+                                Log.d(TAG, "Current location: " + wayLatitude + ", " + wayLongitude);
+                            } else {
+                                Log.d(TAG, "No location");
+                            }
+                        }
+                    }
+                };
+            }
+        });
     }
     @Override
     public void onStart() {
